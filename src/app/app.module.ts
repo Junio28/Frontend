13e788cd953de0ced6c35c5187d845d199d1fe3f
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-19 18:27:58 -0500

    Diseñando listar usuario y el forulario de crear usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './componenets/layouts/main/header/menu/menu.component';
 import { FormLoginComponent } from './componenets/user/form-login/form-login.component';
+import { ListUserComponent } from './componenets/user/list-user/list-user.component';
+import { CopyrightComponent } from './componenets/layouts/main/footer/copyright/copyright.component';
+import { CreateUserComponent } from './componenets/user/create-user/create-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FormLoginComponent
+    FormLoginComponent,
+    ListUserComponent,
+    CopyrightComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
