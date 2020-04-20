ea4eedda2bcde851a603c8c7f92dd1463da04f8d
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-19 21:55:34 -0500

    Create en Clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CopyrightComponent } from './componenets/layouts/main/footer/copyright/
 import { CreateUserComponent } from './componenets/user/create-user/create-user.component';
 import { InfoComponent } from './componenets/layouts/main/content/info/info.component';
 import { ListClientComponent } from './componenets/client/list-client/list-client.component';
+import { CreateClientComponent } from './componenets/client/create-client/create-client.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ListClientComponent } from './componenets/client/list-client/list-clien
     CopyrightComponent,
     CreateUserComponent,
     InfoComponent,
-    ListClientComponent
+    ListClientComponent,
+    CreateClientComponent
   ],
   imports: [
     BrowserModule,
